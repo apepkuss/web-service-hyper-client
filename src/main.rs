488a1e82167c17448e8b2a8bce5e0537d6098a13
Xin Liu <sam@secondstate.io>
2023-09-16 20:17:33 +0800

    feat: add `echo`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,43 @@ use serde_json::json;
 use xin::{
     chat::{
         ChatCompletionRequest, ChatCompletionRequestBuilder, ChatCompletionRequestMessage,
-        ChatCompletionRequestSampling, ChatCompletionRole,
+        ChatCompletionRole,
     },
     completions::{CompletionRequest, CompletionRequestBuilder},
     embeddings::{EmbeddingsRequest, EmbeddingsRequestBuilder},
 };
 
-// static URL_SEND: &str = "http://localhost:8080/send";
-static URL_CHAT_COMPLETIONS: &str = "http://localhost:8080/v1/chat/completions";
-static URL_COMPLETIONS: &str = "http://localhost:8080/v1/completions";
-static URL_EMBEDDINGS: &str = "http://localhost:8080/v1/embeddings";
+static URL_ECHO: &str = "http://localhost:8080/echo";
+static URL_OPENAI_CHAT_COMPLETIONS: &str = "http://localhost:8080/openai/v1/chat/completions";
+static URL_OPENAI_COMPLETIONS: &str = "http://localhost:8080/openai/v1/completions";
+static URL_OPENAI_EMBEDDINGS: &str = "http://localhost:8080/openai/v1/embeddings";
+static URL_LLAMA_CHAT_COMPLETIONS: &str = "http://localhost:8080/llama/v1/chat/completions";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let client = Client::new();
 
-    // ChatCompletionRequest
+    // echo test
     let request = {
         // uri
-        let uri = URL_CHAT_COMPLETIONS.parse::<Uri>()?;
+        let uri = URL_ECHO.parse::<Uri>()?;
+        // data
+        let data = EchoRequest {
+            text: String::from("echo test"),
+        };
+        let data = json!(data);
+        // request
+        Request::builder()
+            .method("POST")
+            .uri(uri)
+            .header("CONTENT_TYPE", "application/json")
+            .body(Body::from(serde_json::to_string(&data)?))?
+    };
+
+    // ChatCompletionRequest
+    let _request = {
+        // uri
+        let uri = URL_OPENAI_CHAT_COMPLETIONS.parse::<Uri>()?;
         // data
         let data = create_chat_request();
         let data = json!(data);
@@ -39,7 +57,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // CreateCompletionRequest
     let _request = {
         // uri
-        let uri = URL_COMPLETIONS.parse::<Uri>()?;
+        let uri = URL_OPENAI_COMPLETIONS.parse::<Uri>()?;
         // data
         let data = create_completion_request();
         let data = json!(data);
@@ -54,7 +72,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // CreateEmbeddingsRequest
     let _request = {
         // uri
-        let uri = URL_EMBEDDINGS.parse::<Uri>()?;
+        let uri = URL_OPENAI_EMBEDDINGS.parse::<Uri>()?;
         // data
         let data = create_embedding_request();
         let data = json!(data);
@@ -64,6 +82,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             .uri(uri)
             .header("CONTENT_TYPE", "application/json")
             .body(Body::from(serde_json::to_string(&data)?))?
+    };
+
+    // models
+    let _request = {
+        // uri
+        let uri = "http://localhost:8080/openai/v1/models".parse::<Uri>()?;
+        // request
+        Request::builder()
+            .method("GET")
+            .uri(uri)
+            .body(Body::empty())?
     };
 
     let response = client.request(request).await?;
@@ -80,6 +109,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 struct SendRequest {
     name: String,
     active: bool,
+}
+
+#[derive(Deserialize, Serialize)]
+struct EchoRequest {
+    text: String,
 }
 
 fn create_chat_request() -> ChatCompletionRequest {
